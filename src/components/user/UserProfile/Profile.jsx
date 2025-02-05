7cf1750e0f8fc3dfc23f61898b3c9af16cb43381
Chandra6388 <cppatel6388@gmail.com>
2025-02-05 11:11:07 +0530

    Replace "Expand Columns" button text with ">>" in FullDataTable component

--- src/components/user/UserProfile/Profile.jsx
+++ src/components/user/UserProfile/Profile.jsx
@@ -7,19 +7,19 @@ const Profile = () => {
         loading: false,
         data: []
     })
-    console.log("chaking active plan name",data);
-    
+    console.log("chaking active plan name", data);
+
 
     const getprofiledata = async () => {
         const data = {
             username: username,
         }
         await Get_Profile_Data(data).then((response) => {
-            console.log("Get_Profile_Data",response);
-            
+            console.log("Get_Profile_Data", response);
+
             if (response.Status) {
-                console.log("Get_Profile_Data",response);
-                
+                console.log("Get_Profile_Data", response);
+
                 localStorage.setItem("expire", false)
                 setData({
                     loading: true,
@@ -27,7 +27,7 @@ const Profile = () => {
                 })
             }
             else {
-                if(response.message === "Client Expired"){
+                if (response.message === "Client Expired") {
                     localStorage.setItem("expire", true)
                 }
                 setData({
@@ -45,72 +45,188 @@ const Profile = () => {
 
 
     return (
-        <div>
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col-lg-4">
-                        <div className="iq-card">
-                            <div className="iq-card-body ps-0 pe-0 pt-0">
-                                <div className="docter-details-block">
-                                    <div
-                                        className="doc-profile-bg bg-primary"
-                                        style={{ height: "46px" }}
-                                    ></div>
-                                    <div className="docter-profile text-center">
+        <>
+            <div>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-lg-4">
+                            <div className="iq-card">
+                                <div className="iq-card-body ps-0 pe-0 pt-0">
+                                    <div className="docter-details-block">
+                                        <div
+                                            className="doc-profile-bg bg-primary"
+                                            style={{ height: "46px" }}
+                                        ></div>
+                                        <div className="docter-profile text-center">
+                                            <img
+                                                src="assets/images/user/11.png"
+                                                alt="profile-img"
+                                                className="avatar-130 img-fluid"
+                                            />
+                                        </div>
+                                        <div className="text-center mt-3 ps-3 pe-3">
+                                            <h4>
+                                                <b>{data?.Username}</b>
+                                            </h4>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-8">
+                            <div className="iq-card">
+                                <div className="iq-card-header d-flex justify-content-between">
+                                    <div className="iq-header-title">
+                                        <h4 className="card-title">Personal Information</h4>
+                                    </div>
+                                </div>
+                                <div className="iq-card-body">
+                                    <div className="about-info m-0 p-0">
+                                        <div className="row">
+                                            <div className="col-4">Username:</div>
+                                            <div className="col-8">{data && data?.data?.Username || "-"} </div>
+                                            <div className="col-4">Mobile No :</div>
+                                            <div className="col-8">{data && data?.data?.Mobile_No || "-"} </div>
+                                            <div className="col-4">Email Id:</div>
+                                            <div className="col-8">{data && data?.data?.EmailId || "-"}</div>
+                                            <div className="col-4">BrokerName :</div>
+                                            <div className="col-8">{data && data?.data?.BrokerName || "-"}</div>
+                                            <div className="col-4">Number of Script :</div>
+                                            <div className="col-8">{data && data?.data?.NumberofScript || "-"}</div>
+                                            <div className="col-4">Group :</div>
+                                            {
+                                                data?.loading && data && data?.data?.Group?.length > 0 ? <div className="col-8">{data && data?.data?.Group.join(' , ')}</div> :
+                                                    <div className="col-8">No Group Available</div>
+                                            }
+                                            <div className="col-4">Plan Name :</div>
+                                            {
+                                                data.loading && data && data?.data?.Planname?.length > 0 ? <div className="col-8">{data && data?.data?.Planname.join(' , ')}</div> :
+                                                    <div className="col-8">No Plan Available</div>
+                                            }
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* <div className="container mt-5">
+
+                        <div
+                            className="row"
+                           
+                        >
+
+                            <div className="col-sm-6 mx-auto"  style={{
+                                boxShadow: "rgba(0, 0, 0, 0.2) 0px 12px 28px 0px, rgba(0, 0, 0, 0.1) 0px 2px 4px 0px, rgba(255, 255, 255, 0.05) 0px 0px 0px 1px inset;",
+                            }}>
+
+                                <div
+
+                                    className="card"
+
+                                    style={{
+
+                                        marginTop: "50px",
+
+                                        // boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px"
+
+                                    }}
+
+                                >
+
+                                    <div className="d-flex justify-content-between">
+
+                                        <h6 className="card-title pt-2 ps-3">Contact</h6>
+
                                         <img
-                                            src="assets/images/user/11.png"
-                                            alt="profile-img"
-                                            className="avatar-130 img-fluid"
+
+                                            className="rounded-circle"
+
+                                            src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png"
+
+                                            alt="Card image cap"
+
+                                            style={{ height: 150, width: 150, marginTop: "-80px" }}
+
                                         />
+
+                                        <h6 className="card-title pt-2 pe-3">Message</h6>
+
                                     </div>
-                                    <div className="text-center mt-3 ps-3 pe-3">
-                                        <h4>
-                                            <b>{data?.Username}</b>
-                                        </h4>
+
+                                    <div className="card-img-top">
+
+                                        <div className="card-body text-center">
+
+                                            <h5 className="card-title">Card title</h5>
+
+                                            <p className="card-text w-50 mx-auto">
+
+                                                Some quick example text to build on the card title and make up the
+
+                                                bulk of the card's content.
+
+                                            </p>
+
+                                            <ul className="d-flex ps-0 justify-content-between w-50 mx-auto">
+
+                                                <li className="list-unstyled">
+
+                                                    <b>76</b>
+
+                                                    <p>post</p>
+
+                                                </li>
+
+                                                <li className="list-unstyled">
+
+                                                    <b>76</b>
+
+                                                    <p>post</p>
+
+                                                </li>
+
+                                                <li className="list-unstyled">
+
+                                                    <b>76</b>
+
+                                                    <p>post</p>
+
+                                                </li>
+
+                                            </ul>
+
+                                            <a href="#" className="btn btn-primary mb-3">
+
+                                                Go somewhere
+
+                                            </a>
+
+                                        </div>
+
                                     </div>
+
                                 </div>
+
                             </div>
+
                         </div>
-                    </div>
-                    <div className="col-lg-8">
-                        <div className="iq-card">
-                            <div className="iq-card-header d-flex justify-content-between">
-                                <div className="iq-header-title">
-                                    <h4 className="card-title">Personal Information</h4>
-                                </div>
-                            </div>
-                            <div className="iq-card-body">
-                                <div className="about-info m-0 p-0">
-                                    <div className="row">
-                                        <div className="col-4">Username:</div>
-                                        <div className="col-8">{data && data?.data?.Username || "-"} </div>
-                                        <div className="col-4">Mobile No :</div>
-                                        <div className="col-8">{data && data?.data?.Mobile_No || "-"} </div>
-                                        <div className="col-4">Email Id:</div>
-                                        <div className="col-8">{data && data?.data?.EmailId || "-"}</div>
-                                        <div className="col-4">BrokerName :</div>
-                                        <div className="col-8">{data && data?.data?.BrokerName || "-"}</div>
-                                        <div className="col-4">Number of Script :</div>
-                                        <div className="col-8">{data && data?.data?.NumberofScript || "-"}</div>
-                                        <div className="col-4">Group :</div>
-                                        {
-                                            data?.loading && data && data?.data?.Group?.length > 0 ? <div className="col-8">{data && data?.data?.Group.join(' , ')}</div> :
-                                                <div className="col-8">No Group Available</div>
-                                        }
-                                        <div className="col-4">Plan Name :</div>
-                                        {
-                                            data.loading && data && data?.data?.Planname?.length > 0 ? <div className="col-8">{data && data?.data?.Planname.join(' , ')}</div> :
-                                                <div className="col-8">No Plan Available</div>
-                                        }
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+
+                    </div> */}
+
+
                 </div>
+
             </div>
 
-        </div>
+
+
+
+
+        </>
+
+
     )
 }
 
