5b4d19ad652636a5a08d6e4bef1c0efe6831ff44
Ashishprajapati2906 <121096675+Ashishprajapati2906@users.noreply.github.com>
2025-02-12 11:34:58 +0530

    admin api work done

--- src/Utils/Config.js
+++ src/Utils/Config.js
@@ -13,4 +13,6 @@ export const base_url = `https://tools.tradestreet.in/backend/`;
 // export const superAdmin_base_url = `${window.location.origin}/superbackend/`
 export const superAdmin_base_url = `https://soptools.tradestreet.in/superbackend/`;
 
+// export const base_url = `http://217.145.69.50:8000/`;
+
 
