9b9dcf561adb6843832e3d14fc757b2db99be727
Chandra6388 <cppatel6388@gmail.com>
2025-03-05 11:39:58 +0530

    done

--- src/components/admin/Plan/AddPlan.js
+++ src/components/admin/Plan/AddPlan.js
@@ -1,3 +1,214 @@
+// import React, { useEffect, useState } from 'react';
+// import axios from 'axios';
+// import Swal from 'sweetalert2'
+// import { AddPlan, GetAllStratgy } from '../../CommonAPI/Admin'
+// import AddForm from "../../../ExtraComponent/FormData";
+// import { useFormik } from "formik";
+// import DropdownMultiselect from "react-multiselect-dropdown-bootstrap";
+// import { useNavigate } from 'react-router-dom';
+
+// const AddPlanPage = () => {
+//     const navigate = useNavigate()
+//     const [selecteOptions, setSelectedOptions] = useState([])
+//     const [selecteScalping, setSelecteScalping] = useState([])
+//     const [selectePattern, setSelectePattern] = useState([])
+//     const [scalpingStratgy, setScalpingStratgy] = useState([])
+//     const [OptionStratgy, setOptionStratgy] = useState([])
+//     const [PatternStratgy, setPatternStratgy] = useState([])
+
+//     useEffect(() => {
+//         GetScalpingStratgy()
+//     }, [])
+
+//     const GetScalpingStratgy = async () => {
+//         await GetAllStratgy()
+//             .then((response) => {
+//                 if (response.Status) {
+//                     setScalpingStratgy(Object.values(response.Scalping))
+//                     setPatternStratgy(Object.values(response.Pattern))
+//                     setOptionStratgy(Object.values(response.Option))
+
+//                 }
+//                 else {
+//                     setScalpingStratgy([])
+//                 }
+//             })
+//             .catch((err) => {
+//                 console.log("Error in getting the Scalping Stratgy", err)
+//             })
+//     }
+
+//     const formik = useFormik({
+//         initialValues: {
+//             NumberofScript: "",
+//             payment: "",
+//             planname: "",
+//             Duration: "",
+//         },
+//         validate: (values) => {
+//             let errors = {};
+//             if (!values.NumberofScript) {
+//                 errors.NumberofScript = "Please Enter Number of Script"
+//             }
+//             if (!values.payment) {
+//                 errors.payment = "Please Enter Payment"
+//             }
+//             if (!values.planname) {
+//                 errors.planname = "Please Enter Plan Name"
+//             }
+//             if (!values.Duration) {
+//                 errors.Duration = "Please Select Duration"
+//             }
+//             return errors;
+//         },
+//         onSubmit: async (values) => {
+//             const req = {
+//                 NumberofScript: values.NumberofScript,
+//                 payment: values.payment,
+//                 planname: values.planname,
+//                 Duration: values.Duration,
+//                 Scalping: selecteScalping,
+//                 Option: selecteOptions,
+//                 PatternS: selectePattern
+//             }
+//             await AddPlan(req)
+//                 .then((response) => {
+//                     if (response.Status) {
+//                         Swal.fire({
+
+//                             title: "Success!",
+//                             text: response.message,
+//                             icon: "success",
+//                             timer: 1500,
+//                             timerProgressBar: true
+//                         });
+//                         setTimeout(() => {
+//                             navigate('/admin/allplan')
+//                         }, 1500)
+//                     }
+//                     else {
+//                         Swal.fire({
+
+//                             title: "Error!",
+//                             text: response.message,
+//                             icon: "error",
+//                             timer: 1500,
+//                             timerProgressBar: true
+//                         });
+//                     }
+//                 })
+//                 .catch((err) => {
+//                     console.log("Error in adding the new user", err)
+//                 })
+//         },
+//     });
+
+//     const fields = [
+//         {
+//             name: "NumberofScript",
+//             label: "Number of Script",
+//             type: "text3",
+//             label_size: 12,
+//             hiding: false,
+//             col_size: 6,
+//             disable: false,
+//         },
+//         {
+//             name: "payment",
+//             label: "Payment",
+//             type: "text3",
+//             label_size: 12,
+//             hiding: false,
+//             col_size: 6,
+//             disable: false,
+//         },
+//         {
+//             name: "planname",
+//             label: "Plan Name",
+//             type: "text",
+//             label_size: 12,
+//             hiding: false,
+//             col_size: 6,
+//             disable: false,
+//         }, 
+//         {
+//             name: "Duration",
+//             label: "Duration",
+//             type: "select1",
+//             options: [
+//                 { value: "One_Month", label: "One Month" },
+//                 { value: "Quarterly", label: "Quarterly" },
+//                 { value: "Half_Yearly", label: "Half Yearly" },
+//                 { value: "Yearly", label: "Yearly" },
+//             ],
+//             label_size: 12,
+//             hiding: false,
+//             col_size: 6,
+//             disable: false,
+//         },
+//     ];
+
+//     return (
+//         <>
+//             <AddForm
+//                 fields={fields.filter(
+//                     (field) => !field.showWhen || field.showWhen(formik.values)
+//                 )}
+//                 page_title="Add Plan"
+//                 btn_name="Add"
+//                 btn_name1="Cancel"
+//                 formik={formik}
+//                 btn_name1_route={"/admin/clientservice"}
+//                 additional_field={
+//                     <>
+//                         {scalpingStratgy && scalpingStratgy.length > 0 && (
+//                             <div className="col-lg-6 mt-2 ">
+//                                 <h6>Scalping</h6>
+//                                 <DropdownMultiselect
+//                                     options={scalpingStratgy}
+//                                     name="groupName"
+//                                     handleOnChange={(selected) => {
+//                                         setSelecteScalping(selected);
+//                                     }}
+//                                 />
+//                             </div>
+//                         )}
+
+//                         {OptionStratgy && OptionStratgy.length > 0 && (
+//                             <div className="col-lg-6 mt-2 ">
+//                                 <h6>Option</h6>
+//                                 <DropdownMultiselect
+//                                     options={OptionStratgy}
+//                                     name="groupName"
+//                                     handleOnChange={(selected) => {
+//                                         setSelectedOptions(selected);
+//                                     }}
+//                                 />
+//                             </div>
+//                         )}
+
+//                         {PatternStratgy && PatternStratgy.length > 0 && (
+//                             <div className="col-lg-6 mt-2  ">
+//                                 <h6>Patterm</h6>
+//                                 <DropdownMultiselect
+//                                     options={PatternStratgy}
+//                                     name="groupName"
+//                                     handleOnChange={(selected) => {
+//                                         setSelectePattern(selected);
+//                                     }}
+//                                 />
+//                             </div>
+
+//                         )}
+//                     </>
+//                 }
+//             />
+
+//         </>
+//     );
+// };
+
+// export default AddPlanPage;
 
 
 import React, { useEffect, useState } from 'react';
@@ -7,8 +218,6 @@ import AddForm from "../../../ExtraComponent/FormData";
 import { useFormik } from "formik";
 import Select from 'react-select';
 import { useNavigate } from 'react-router-dom';
-import Content from '../../../ExtraComponent/Content';
-
 
 // Custom multi-select component
 const CustomMultiSelect = ({ label, options, selected, onChange, disabled }) => {
@@ -191,7 +400,7 @@ const AddPlanPage = () => {
             name: "PlanType",
             label: "Plan Type",
             type: "select",
-            options: [{ value: "Scalping", label: "SOP" }, { value: "Charting", label: "Charting" }],
+            options: [{ value: "Scalping", label: "Scalping" }, { value: "Charting", label: "Charting" }],
             col_size: 6,
         },
         {
@@ -213,18 +422,18 @@ const AddPlanPage = () => {
             type: "text",
             col_size: 6,
         },
-        {
-            name: "LiveTradeAccount",
-            label: "Live Trade Account",
-            type: "text",
-            col_size: 6,
-        },
-        {
-            name: "PaperTradeAccount",
-            label: "Paper Trade Account",
-            type: "text",
-            col_size: 6,
-        },
+        // {
+        //     name: "LiveTradeAccount",
+        //     label: "Live Trade Account",
+        //     type: "text",
+        //     col_size: 6,
+        // },
+        // {
+        //     name: "PaperTradeAccount",
+        //     label: "Paper Trade Account",
+        //     type: "text",
+        //     col_size: 6,
+        // },
         {
             name: "Duration",
             label: "Duration",
@@ -241,15 +450,9 @@ const AddPlanPage = () => {
 
     return (
 
-        <Content
-            Page_title={" ➕ Add Plan"}
-            button_status={false}
-            backbutton_status={false}
-        >
-
-
         <AddForm
             fields={fields.filter((field) => !field.showWhen || field.showWhen(formik.values))}
+            page_title="Add Plan"
             btn_name="Add"
             btn_name1="Cancel"
             formik={formik}
@@ -259,7 +462,7 @@ const AddPlanPage = () => {
                     {formik.values.PlanType == "Charting" && (
                         <>
                             <CustomMultiSelect
-                                label={<span className='card-text-Color' >Segment</span>}
+                                label={<span >Segment</span>}
                                 options={[
                                     { value: "Cash", label: "Cash" },
                                     { value: "Future", label: "Future" },
@@ -274,7 +477,7 @@ const AddPlanPage = () => {
 
                     {formik.values.PlanType == "Scalping" && (
                         <CustomMultiSelect
-                            label={<span className='card-text-Color' >Scalping</span>}
+                            label={<span >Scalping</span>}
                             options={scalpingStratgy.map(strategy => ({ value: strategy, label: strategy }))}
                             selected={selecteScalping}
                             onChange={(selected) => handleSelectChange("scalping", selected)}
@@ -283,7 +486,7 @@ const AddPlanPage = () => {
 
                     {formik.values.PlanType == "Scalping" && (
                         <CustomMultiSelect
-                            label={<span className='card-text-Color' >Option</span>}
+                            label={<span >Option</span>}
                             options={OptionStratgy.map(strategy => ({ value: strategy, label: strategy }))}
                             selected={selecteOptions}
                             onChange={(selected) => handleSelectChange("option", selected)}
@@ -292,7 +495,7 @@ const AddPlanPage = () => {
 
                     {formik.values.PlanType == "Scalping" && (
                         <CustomMultiSelect
-                            label={<span className='card-text-Color'>Pattern</span>}
+                            label={<span >Pattern</span>}
                             options={PatternStratgy.map(strategy => ({ value: strategy, label: strategy }))}
                             selected={selectePattern}
                             onChange={(selected) => handleSelectChange("pattern", selected)}
@@ -301,7 +504,6 @@ const AddPlanPage = () => {
                 </>
             }
         />
-        </Content>
     );
 };
 
