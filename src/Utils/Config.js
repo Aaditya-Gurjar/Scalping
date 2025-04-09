2539609aef71e71995d5864c995cd80866e06966
Aaditya-Gurjar <aaditya2002.com@gmail.com>
2025-04-09 18:02:22 +0530

    Refactor Config.js to uncomment base_url for active use and comment out previous URL for clarity.

--- src/Utils/Config.js
+++ src/Utils/Config.js
@@ -1,4 +1,4 @@
-// export const base_url = `${window.location.origin}/backend/`
+export const base_url = `${window.location.origin}/backend/`
 // export const base_url = `http://193.239.237.144:8002/`;
 // export const base_url = `http://193.239.237.147:8000/`;
 // export const base_url = `http://193.239.237.149:8000/`;
@@ -13,7 +13,7 @@
 // export const base_url = `https://tools.nextbrand.tech/backend/`;
 // export const base_url = `https://tools.scalpingalgo.in//backend/`;
 // export const base_url = `https://tools.easytrades.in/backend/`;
-export const base_url = `https://tools.foxittechnology.com/backend/`;
+// export const base_url = `https://tools.foxittechnology.com/backend/`;
 // if superAdmin login then change the url only for it
 // export const superAdmin_base_url = `${window.location.origin}/superbackend/`
 
