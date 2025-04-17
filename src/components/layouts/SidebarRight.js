2f6b465e12f7b2d817859981e7c1d7d7449a14b3
Aaditya-Gurjar <aaditya2002.com@gmail.com>
2025-04-17 14:10:15 +0530

    Add Master Account entry to sidebar with improved label formatting

--- src/components/layouts/SidebarRight.js
+++ src/components/layouts/SidebarRight.js
@@ -89,11 +89,15 @@ const Sidebar = ({ position }) => {
         label: "ApiCreateInfo",
       },
 
-      // {
-      //   path: "/admin/Master-Account",
-      //   icon: <i className="la la-user-shield"></i>,  
-      //   label: "Master Account",
-      // },
+      {
+        path: "/admin/Master-Account",
+        icon: <i className="la la-user-shield"></i>,  
+        label: (
+          <>
+            Master<br />Account
+          </>
+        ),
+      },
     ],
     Superadmin: [
       {
