1b5e634f996d90b103ddcc89880139181fe9f7f2
Ashishprajapati2906 <121096675+Ashishprajapati2906@users.noreply.github.com>
2025-04-28 11:02:59 +0530

    done

--- src/Utils/Config.js
+++ src/Utils/Config.js
@@ -1,9 +1,9 @@
-export const base_url = `${window.location.origin}/backend/`
+// export const base_url = `${window.location.origin}/backend/`
 // export const base_url = `http://193.239.237.144:8002/`;
 // export const base_url = `http://193.239.237.147:8000/`;
 // export const base_url = `http://193.239.237.149:8000/`;
 // export const base_url = `http://217.145.69.50:8000/`;
-// export const base_url = `http://217.145.69.58:8000/`;
+export const base_url = `http://217.145.69.58:8000/`;
 
 // 
 // export const base_url = `http://217.145.69.67:8000/`;
