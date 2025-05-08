286cb6088c69830242913f52f630904186580a15
NehaSharma3496 <sharmaneha3496@gmail.com>
2025-05-08 15:35:13 +0530

    Enhance MasterAccount component: update Child Accounts header styling for improved readability and consistency

--- src/components/admin/MasterAccount/MasterAccount.jsx
+++ src/components/admin/MasterAccount/MasterAccount.jsx
@@ -305,7 +305,7 @@ const MasterAccount = () => {
           <h4 className="account-title-single">Master Account :  
           <span className="account-value-single text-dark">{data.MainUser}</span></h4>
           <hr className="account-divider" />
-          <h4 className="account-title-single">Child Accounts     <button className="addbtn btn btn-sm" onClick={handleEditClick}>
+          <h4 className="fs-6">Child Accounts     <button className="addbtn btn btn-sm" onClick={handleEditClick}>
            <i className='fa fa-edit me-0'></i> 
           </button></h4>
           <div className="account-grid-single">
